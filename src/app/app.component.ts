77889e280ce37bf4ba96b8313846dd3ea401518e
FernandoAGC <fernando.a.galicia.c@gmail.com>
2024-02-20 12:38:29 -0600

    update: main layout
    Header and products grid placed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ProductCardsGridComponent } from './components/product-cards-grid/product-cards-grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ToolbarComponent, ProductCardsGridComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
