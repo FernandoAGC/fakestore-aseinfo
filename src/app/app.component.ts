f30cb2df6c65af8fff1bdb0e5494471fd95355a1
FernandoAGC <fernando.a.galicia.c@gmail.com>
2024-02-20 16:45:46 -0600

    add: toolbar added in the layout
    A product-toolbar component was placed in the layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { ProductCardsGridComponent } from './components/product-cards-grid/product-cards-grid.component';
+import { ProductToolbarComponent } from './components/product-toolbar/product-toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ToolbarComponent, ProductCardsGridComponent],
+  imports: [CommonModule, RouterOutlet, ToolbarComponent, ProductCardsGridComponent, ProductToolbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
