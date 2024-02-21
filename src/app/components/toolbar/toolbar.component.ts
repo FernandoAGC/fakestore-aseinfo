3ca702575fb5cf55da510e7aed974dd43c045dc6
FernandoAGC <fernando.a.galicia.c@gmail.com>
2024-02-21 16:14:25 -0600

    update: header toolbar with cart icon
    Added a cart icon with included items badge to the header toolbar. Also time is sticky.

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatBadgeModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
+
+  items: number = 0;
 
 }
